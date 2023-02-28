7f74efb4406c0978f8eeb6b33cee41ea4fb6cd54
GustavoCMatias <gustavocmatias@gmail.com>
2023-02-28 19:28:12 -0300

    feat/Rota DELETE  /urls/:id configurada

--- src/controller/url.controller.js
+++ src/controller/url.controller.js
@@ -2,41 +2,63 @@ import { nanoid } from "nanoid"
 import { db } from "../database/database.connection.js"
 
 
-export async function shortenUrl(req, res){
+export async function shortenUrl(req, res) {
     const userId = res.locals.user
-    const {url} = req.body
+    const { url } = req.body
 
-    try{
+    try {
         const shortUrl = nanoid(8)
-        const {rows} = await db.query('INSERT INTO links (original_link, short_link, user_id) VALUES ($1, $2, $3) RETURNING id', [url, shortUrl, userId])
+        const { rows } = await db.query('INSERT INTO links (original_link, short_link, user_id) VALUES ($1, $2, $3) RETURNING id', [url, shortUrl, userId])
         const id = rows[0].id
-        res.status(201).send({id, shortUrl})
-    }catch (error) {
+        res.status(201).send({ id, shortUrl })
+    } catch (error) {
         res.status(500).send(error.message)
     }
-    
+
 }
 
-export async function getUrlFromId(req, res){
-    const {id} = req.params
-    try{
-        const {rows, rowCount} = await db.query('SELECT id, short_link, original_link FROM links WHERE id = $1', [id])
-        if(rowCount === 0) return res.sendStatus(404)
+export async function getUrlFromId(req, res) {
+    const { id } = req.params
+    try {
+        const { rows, rowCount } = await db.query('SELECT id, short_link, original_link FROM links WHERE id = $1', [id])
+        if (rowCount === 0) return res.sendStatus(404)
         res.status(200).send({
             id,
             shortUrl: rows[0].short_link,
             url: rows[0].original_link
         })
-    }catch (error) {
+    } catch (error) {
         res.status(500).send(error.message)
     }
 }
 
-export async function getUrlFromShortUrl(req, res){
-    const {shortUrl} = req.params
+export async function getUrlFromShortUrl(req, res) {
+    const { shortUrl } = req.params
+    try {
+        const { rows, rowCount } = await db.query('UPDATE links SET view_count = view_count+1 WHERE short_link = $1 RETURNING original_link', [shortUrl])
 
-    const {rows, rowCount} = await db.query('UPDATE links SET view_count = view_count+1 WHERE short_link = $1 RETURNING original_link', [shortUrl])
+        if (rowCount === 0) return res.sendStatus(404)
+        res.redirect(302, rows[0].original_link)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+    
+}
 
-    if(rowCount === 0)return res.sendStatus(404)
-    res.redirect(302, rows[0].original_link)
+export async function deleteUrl(req, res) {
+    const { id } = req.params
+    const userId = res.locals.user
+
+    try {
+        const { rows, rowCount } = await db.query('SELECT user_id FROM links WHERE id = $1', [id])
+
+        if (rowCount === 0) return res.sendStatus(404)
+        if (rows[0].user_id !== userId) return res.sendStatus(401)
+
+        await db.query('DELETE FROM links WHERE id = $1', [id])
+        res.sendStatus(204)
+    } catch (error) {
+        res.status(500).send(error.message)
+    }
+    
 }
--- src/router/url.router.js
+++ src/router/url.router.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getUrlFromId, getUrlFromShortUrl, shortenUrl } from "../controller/url.controller.js";
+import { deleteUrl, getUrlFromId, getUrlFromShortUrl, shortenUrl } from "../controller/url.controller.js";
 import tokenValidation from "../middleware/authValidation.middleware.js";
 import { validarUrlSchema } from "../middleware/url.middleware.js";
 import { urlSchema } from "../schema/url.schema.js";
@@ -11,5 +11,6 @@ const urlRouter = Router()
 urlRouter.post('/urls/shorten', validarUrlSchema(urlSchema), tokenValidation, shortenUrl)
 urlRouter.get('/urls/:id', getUrlFromId)
 urlRouter.get('/urls/open/:shortUrl', getUrlFromShortUrl)
+urlRouter.delete('/urls/:id', tokenValidation, deleteUrl)
 
 export default urlRouter
